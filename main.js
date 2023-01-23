50bba02f40bc14199c4e10bc6d515dfd42d28678
OrzzzAL <93055773+OrzzzAL@users.noreply.github.com>
2023-01-22 18:07:45 -0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
 ASSET_MANAGER.queueDownload("./Sprites/main.png");
 ASSET_MANAGER.queueDownload("./Sprites/bg1.png");
-
+ASSET_MANAGER.queueDownload("./shooter/WR.png");
+ASSET_MANAGER.queueDownload("./shooter/dead.png");
+ASSET_MANAGER.queueDownload("./shooter/BR.png");
+ASSET_MANAGER.queueDownload("./shooter/shooting.png");
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
@@ -12,6 +15,7 @@ ASSET_MANAGER.downloadAll(() => {
 
 	gameEngine.init(ctx);
 	gameEngine.addEntity(new Zero(gameEngine));
+        gameEngine.addEntity(new shooter(gameEngine));
 	gameEngine.addEntity(new bg1(gameEngine));
 
 	gameEngine.start();
