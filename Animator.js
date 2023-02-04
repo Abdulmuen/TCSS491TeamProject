6259a1145fd6eee091408f22baf5328e698089c5
Abdulmuen <97244586+Abdulmuen@users.noreply.github.com>
2023-02-04 14:42:52 -0800

    Update Animator.js

--- Animator.js
+++ Animator.js
@@ -1,38 +1,47 @@
 class Animator {
-    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration, backwards,loop) {
-        Object.assign(this, {spritesheet, xStart, yStart, width, height, frameCount, frameDuration, backwards,loop});
+    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration, backwards, loop, isPlayer) {
+        Object.assign(this, {spritesheet, xStart, yStart, width, height, frameCount, frameDuration, backwards, loop, isPlayer});
         this.elapsedTime = 0;
         this.totalTime = frameCount * frameDuration;
 
     }
 
-    drawFrame(tick, ctx, x, y,scale) {
+    drawFrame(TICK, ctx, x, y, scale) {
 
-        this.elapsedTime += tick;
-         if (this.isDone()) {
+        const frame = this.currentFrame();
+
+        if (this.isPlayer) {
+            this.elapsedTime += TICK * params.playerSpeed;
+        } else {
+            this.elapsedTime += TICK * params.NPCSpeed;
+        }
+
+        if (this.isDone()) {
             if (this.loop) {
                 this.elapsedTime -= this.totalTime;
             } else {
+                if(this.backwards){
+                    ctx.save();
+                    ctx.scale(-1,1);
+                    ctx.drawImage(this.spritesheet, this.xStart + this.width * frame, this.yStart, this.width, this.height, -x - this.width, y, this.width * scale, this.height * scale)
+                    ctx.restore();
+                } else {
+                    ctx.drawImage(this.spritesheet, this.xStart + this.width * frame, this.yStart, this.width, this.height, x, y, this.width * scale, this.height * scale)
+                }
                 return;
             }
         }
 
-        if(this.elapsedTime > this.totalTime) reset();
-        const frame = this.currentFrame();
-
         if(this.backwards){
             ctx.save();
             ctx.scale(-1,1);
-
             ctx.drawImage(this.spritesheet, this.xStart + this.width * frame, this.yStart, this.width, this.height, -x - this.width, y, this.width * scale, this.height * scale)
             ctx.restore();
-        }else
+        } else {
             ctx.drawImage(this.spritesheet, this.xStart + this.width * frame, this.yStart, this.width, this.height, x, y, this.width * scale, this.height * scale)
+        }
+    }
 
-    }
-    reset() {
-        this.elapsedTime -= this.totalTime;
-    }
     currentFrame() {
         return Math.floor(this.elapsedTime / this.frameDuration);
     }
