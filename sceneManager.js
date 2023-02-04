f87c39b67c57f9ba8f491a94c59b3ee5d359b8f1
Abdulmuen <97244586+Abdulmuen@users.noreply.github.com>
2023-02-04 14:44:19 -0800

    Add files via upload

--- sceneManager.js
+++ sceneManager.js
@@ -20,10 +20,19 @@ class SceneManager {
         this.Zero.y = y;
 
         this.game.addEntity(this.Zero);
+        gameEngine.addEntity(new bg1(gameEngine, this.x));
+        gameEngine.addEntity(new gr1(gameEngine, this.x));
+        gameEngine.addEntity(new shooter(gameEngine, 1160, 255));
+        gameEngine.addEntity(new shooter(gameEngine, 1345, 255));
+        //gameEngine.addEntity(new boss((gameEngine,1400,177.5)));
     }
 
     update() {
     
+        let midpoint = PARAMS.CANVAS_WIDTH/2;
+
+        if (this.x < this.Zero.x - midpoint) this.x = this.Zero.x - midpoint;
+
     }
 
     draw(ctx) {
